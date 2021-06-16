d10e381959caa9d1321bdf86f40deaecf49ee87c
Supervodka <supervodka666@gmail.com>
2021-06-16 20:28:44 +0300

    //start

---
+++ Project2/Project2/ClientApp/src/components/JavaScript.js
@@ -0,0 +1,33 @@
+﻿import { type } from 'jquery';
+import React, { Component } from 'react';
+
+export class goose extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { currentCount: 0 };
+        this.click = this.click.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.currentCount}</h1> //получение текущей CurrentCount//
+                <p>This is a simple example of white goose.</p>
+
+                <input type="button" onClick={this.click} /> //создание кнопки и привязка к методу click(при каждом нажатии на кнопку выполняется метод click)
+                
+
+            </div>
+        );
+    }
+
+    async click() {
+        const response = await fetch('Goose');// получение с GooseController
+        const data = await response.json();//расшифровка данных(парсинг)??
+        this.setState({ currentCount: data });//отправка даты в CurrentCount
+
+    }
+   
+}
+
---
+++ Project2/Project2/Controllers/GooseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GooseController : ControllerBase
+
+    {
+        
+        [HttpGet] //HTTP-get запрос
+        public string Get(string mal) //метод получения запрос
+        {
+            mal = "lol";
+            return mal;
+        }
+       
+        
+        
+    }
+}
