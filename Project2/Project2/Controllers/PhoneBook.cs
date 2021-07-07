1ede4d9062d9e9566d45bca535a6522423d33299
v.gavrilchik <v.gavrilchik@itransition.com>
2021-07-07 21:11:43 +0300

    Refactoring & review

--- Project2/Project2/Controllers/PhoneBook.cs
+++ Project2/Project2/Controllers/PhoneBook.cs
@@ -2,88 +2,63 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Project2.Controllers
 {
     [ApiController]                //указываем что использует расширения НТТР АРI
     [Route("[controller]")]
-
     public class PhoneBook : ControllerBase            // заимствование базового Model-Viev-controller'a без визуала
     {
-
-      
-
         [HttpPost] //определяет которое поддерживает ХТТП пост метод
         [Route("[action]")]
-        
-
         public void AddNew (ContactModel contact)
         {
-            using (ContactContext db = new ContactContext())
-            {
-                db.Contacts.Add(contact);
-                db.SaveChangesAsync();
-                Console.WriteLine("Сохранил");
-            }
+            using var db = new ContactContext();
+            db.Contacts.Add(contact);
+            db.SaveChangesAsync();
+
+            Console.WriteLine("Сохранил");
         }
 
         [HttpPut] //определяет которое поддерживает ХТТП пост метод
         [Route("[action]")]
-
         public void Update (ContactModel contact)
         {
-            using (ContactContext db = new ContactContext())
-            {
-                db.Contacts.Update(contact);
-                db.SaveChangesAsync();
-                Console.WriteLine("Обновил");
-            }
+            using var db = new ContactContext();
+            db.Contacts.Update(contact);
+            db.SaveChangesAsync();
+
+            Console.WriteLine("Обновил");
         }
+
         [HttpGet] //определяет которое поддерживает ХТТП пост метод
         [Route("[action]")]
-
         public List<ContactModel> Get ( ContactModel contact)
         {
-            using (ContactContext db = new ContactContext())
-            {
-                db.Contacts.ToList();
-                return db.Contacts.ToList();
-            }
+            using var db = new ContactContext();
 
+            return db.Contacts.ToList();
         }
 
         [HttpDelete] //определяет которое поддерживает ХТТП пост метод
         [Route("[action]")]
-
-        public void Remove(int Id)
+        public void Remove(int id)
         {
-            using( ContactContext db = new ContactContext())
-            {
-                var c = (db.Contacts.First(c => c.Id == Id));
-                db.Contacts.Remove(c);
-                db.SaveChanges();
-                Console.WriteLine($"ты удалил элемент {Id}");
-            }
-        }
+            using var db = new ContactContext();
+            var contactModelToRemove = (db.Contacts.Single(contact => contact.Id == id));
+            db.Contacts.Remove(contactModelToRemove);
+            db.SaveChanges();
 
+            Console.WriteLine($"ты удалил элемент {id}");
+        }
 
         [HttpGet] //определяет которое поддерживает ХТТП пост метод
         [Route("[action]")]
-
-
-        public List<ContactModel> Search(string name )
+        public List<ContactModel> Search(string name)
         {
-            using (ContactContext db = new ContactContext())
-            {
-                var c = (db.Contacts.Where(c => c.ContactName.Contains(name)).ToList());
-               
+            using var db = new ContactContext();
 
-                return (List<ContactModel>)c;
-            }    
+            return db.Contacts.Where(contact => contact.ContactName.Contains(name)).ToList();
         }
     }
-
-    
-
 }
